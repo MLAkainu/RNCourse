52eb4f8cdf4dda380566193c83fb9c0b0488eedb
MLITBKU <loc.nguyenminh1605@hcmut.edu.vn>
2024-01-18 09:07:26 +0700

    add screen background

--- real-time-app/screens/StartGameScreen.js
+++ real-time-app/screens/StartGameScreen.js
@@ -1,13 +1,54 @@
-import { TextInput, Button, View, StyleSheet} from 'react-native';
-
+import { useState} from 'react';
+import { TextInput, Button, View, StyleSheet, Alert} from 'react-native';
+import Colors from '../constants/color';
 import PrimaryButton from '../components/PrimaryButton';
 
-function StartGameScreen() {
+function StartGameScreen({onPickNumber}) {
+    const [enteredNumber, setEnteredNumber] = useState('');
+    function numberInputHandler(enteredText) {
+        setEnteredNumber(enteredText);
+    }
+
+    function resetInputHandler() {
+        setEnteredNumber('');
+    }
+
+    function confirmInputHandler() {
+        const chosenNumber = parseInt(enteredNumber);
+
+        if (isNaN(chosenNumber) || chosenNumber <= 0 || chosenNumber > 99) {
+            Alert.alert('Invalid number', 'Number has to be a number between 1 and 99', 
+            [{text: 'Okay', style: 'destructive', onPress: resetInputHandler}]
+            );
+            return;
+        }
+
+        onPickNumber(chosenNumber);
+    }
+
+
     return (
     <View style={styles.inputContainer}> 
-        <TextInput style={styles.numberInput} maxLength={2} />
-        <PrimaryButton>Reset</PrimaryButton>
-        <PrimaryButton>Confirm</PrimaryButton>
+        <TextInput 
+        style={styles.numberInput} 
+        maxLength={2} 
+        keyboardType="number-pad"
+        autoCapitalize="none"
+        autoCorrect={false}
+        onChangeText={numberInputHandler}
+        value={enteredNumber}
+        />
+        <View style={styles.buttonsContainer}>
+            <View style={styles.buttonContainer}>
+                <PrimaryButton onPress={resetInputHandler}>Reset</PrimaryButton>
+                
+            </View>
+            <View style={styles.buttonContainer}>
+                <PrimaryButton onPress={confirmInputHandler}>Confirm</PrimaryButton>
+                
+            </View>
+
+        </View>
     </View>
     );
 }
@@ -16,28 +57,38 @@ export default StartGameScreen;
 
 const styles = StyleSheet.create({
     inputContainer: {
+        justifyContent: 'center',
+        alignItems: 'center',
         marginTop: 100,
         marginHorizontal: 24,
         paddding: 16,
-        backgroundColor: '#72063c',
+        backgroundColor: Colors.primary800,
         borderRadius: 8,
         elevation: 4,
         shadowColor: 'black',
         shadowOffset: { width: 0, height: 2},
         shadowRadius: 6,
         shadowOpacity: 0.25,
+        
     },
     numberInput: {
         height: 50,
         width: 50,
         fontSize: 32,
-        borderBottomColor: '#ddb52f',
+        borderBottomColor: Colors.accent500,        
         borderBottomWidth: 2,
-        color: '#ddb52f',
+        color: Colors.accent500,
         marginVertical: 8,
         fontWeight: 'bold',
         textAlign: 'center',
-    }
+    },
+    buttonsContainer: {
+        flexDirection: 'row',
+
+    },
+    buttonContainer: {
+        flex: 1,
+    },
 
 
 });
